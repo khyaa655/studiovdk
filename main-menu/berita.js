cce03b369c5e122c011d8ae989a1ae07efe96dca
Viko Dwi Kurniawan <85740085+vikodk67@users.noreply.github.com>
2021-08-18 20:30:49 +0700

    Add files via upload

--- main-menu/berita.js
+++ main-menu/berita.js
@@ -1,25 +1,4 @@
-function vikoberita() {
-    fetch('https://viko-api.herokuapp.com/api/info/covidworld?apikey=rxking')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
 
-
-        let sdss = data.result.totalCases
-        let pulih = data.result.recovered
-        let mati = data.result.deaths
-        let aktip = data.result.activeCases
-        let clos = data.result.closedCases
-        let updt = data.result.lastUpdate
-        document.getElementById("data-bs").innerHTML = sdss
-        document.getElementById("data-pulih").innerHTML = pulih
-        document.getElementById("data-ningoy").innerHTML = mati
-        document.getElementById("data-upp").innerHTML = updt
-    })
-    .catch(err => {
-        console.log(err)
-    });
-}
 function igstalk() {
     fetch('https://viko-api.herokuapp.com/api/stalk/ig?apikey=rxking&query=vikodwik_rmx')
     .then(response => response.json())
@@ -33,6 +12,22 @@ function igstalk() {
         document.getElementById("followw").innerHTML = ledalede
         document.getElementById("foll").innerHTML = polo
         document.getElementById("beck").innerHTML = ledal
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+function vikoapi() {
+    fetch('https://viko-api.herokuapp.com/api/random/informasi?&apikey=rxking')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let intejer = data.result.infoku
+        let inpo = data.result.bug
+        document.getElementById("crito").innerHTML = inpo
+
+        document.getElementById("informasi").innerHTML = intejer
     })
     .catch(err => {
         console.log(err)
