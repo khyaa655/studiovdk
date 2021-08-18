4a2345e856d95b6f2291ad9e329091d03f473e6f
Viko Dwi Kurniawan <85740085+vikodk67@users.noreply.github.com>
2021-08-18 17:59:50 +0700

    Add files via upload

---
+++ api/ingfoku.php
@@ -0,0 +1,5 @@
+<?php
+$sapa= array('infoku' => "selamat datang di situs studiovdk, kang ngoding kopas, kami melayani free rest-api dan konten lainnya", 'bug' => "perbaikan viko-api.herokuapp.com mengenai buffer gambar");
+$str= array('result' => $sapa);
+echo json_encode($str);
+?>
