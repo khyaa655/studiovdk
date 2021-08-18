933d73b60e34c3e0a98b03c9fc0eb3c0ea0f3355
Viko Dwi Kurniawan <85740085+vikodk67@users.noreply.github.com>
2021-08-18 19:09:56 +0700

    Update ingfoku.php

--- api/ingfoku.php
+++ api/ingfoku.php
@@ -1,5 +1,16 @@
 <?php
-$sapa= array('infoku' => "selamat datang di situs studiovdk, kang ngoding kopas, kami melayani free rest-api dan konten lainnya", 'bug' => "perbaikan viko-api.herokuapp.com mengenai buffer gambar");
+date_default_timezone_set('Asia/Jakarta');
+$jam=date("G");
+if($jam>=0&&$jam<=10)
+$sapa= ("selamat pagi");
+else if($jam>=11&&$jam<=13)
+$sapa= ("selamat siang");
+else if($jam>=14&&$jam<=17)
+$sapa= ("selamat sore");
+else if($jam>=18&&$jam<=23)
+$sapa= ("selamat malam");
+
+$sapa= array('infoku' => "selamat datang di situs studiovdk dan $sapa, kang ngoding kopas kami melayani free rest-api dan konten lainnya menyusul soalnya lagi update. WA wa.me/6281515958390 ", 'bug' => "perbaikan viko-api.herokuapp.com mengenai buffer gambar");
 $str= array('result' => $sapa);
 echo json_encode($str);
 ?>
